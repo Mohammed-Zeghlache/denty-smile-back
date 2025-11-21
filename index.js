8c0fd5b729313fd1d301be639e13f4bd981b516f
Mohamed <mouhamedzeghlache7@gmail.com>
2025-11-21 11:09:11 +0100

    first commit

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ app.get('/api/appointments/phone/:phone', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
